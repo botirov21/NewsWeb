cf167567cdb4c912ae9df73550634c2a7509775a
MurtazaxanovM <murtazaxanovm@gmail.com>
2022-05-25 16:22:52 +0500

    fnd

--- NewsWeb/Controllers/HomeController.cs
+++ NewsWeb/Controllers/HomeController.cs
@@ -11,6 +11,57 @@ namespace NewsWeb.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Categori()
+        {
+            return View();
+
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+
+        }
+
+        public IActionResult Elements()
+        {
+            return View();
+
+        }
+        public IActionResult Latest_news()
+        {
+            return View();
+
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+
+        }
+
+        public IActionResult Single_blog()
+        {
+            return View();
+
+        }
+
 
     }
 }
